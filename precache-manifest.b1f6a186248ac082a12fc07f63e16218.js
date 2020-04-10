050c9b9d32a21e098b145a9c4e8e8dc4b8bc10b7
Dong Le <dong.le@miracles.vn>
2020-04-10 12:45:39 +0700

    Updates

---
+++ precache-manifest.b1f6a186248ac082a12fc07f63e16218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f3db065bcc26284567bcdf2382fe1b",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d16f21fb279c5449905d",
+    "url": "/covid-19/static/css/main.58346429.chunk.css"
+  },
+  {
+    "revision": "1898267552f7c6ebc70a",
+    "url": "/covid-19/static/js/2.2e6e1192.chunk.js"
+  },
+  {
+    "revision": "2bfe6bc3475f9fbb190566549a478500",
+    "url": "/covid-19/static/js/2.2e6e1192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16f21fb279c5449905d",
+    "url": "/covid-19/static/js/main.759adb39.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "f71fcc01abc279d58a5dd0fc3c0db9a2",
+    "url": "/covid-19/static/media/virusmini.f71fcc01.png"
+  }
+]);
